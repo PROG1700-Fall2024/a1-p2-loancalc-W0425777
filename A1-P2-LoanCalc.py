b77738979bf7a52c6b3336771e11269a9243d819
w0425777 <w0425777@nscc.ca>
2024-10-04 13:55:28 -0300

    Finished

--- A1-P2-LoanCalc.py
+++ A1-P2-LoanCalc.py
@@ -1,14 +1,27 @@
-#Program 2 – Weekly Loan Calculator
-#Develop a short term loan calculator program as a console application 
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
+    #print name
+    print("\nWeekly Loan Calculator\n")
 
+    #input for a (amount borrowed)
+    A = float(input("Enter the amount of the loan: "))
 
+    #input for r (interest)
+    r = float(input("Enter the interest rate (%): "))
 
+    #input for n (years)
+    n = float(input("Enter the number of years: "))
 
+    #math r/5200 = i
+    i = (r / 5200)
 
+    #math weekly pay =  (i/1-(1+i) to the power of -52*n) * a
+    weekly = (i/(1-(1+i)**(-52*n)))*A
+
+    #print weekly 
+    print("\nYour weekly payment will be: ${0:.2f}\n".format(weekly))
 
 
     # YOUR CODE ENDS HERE
